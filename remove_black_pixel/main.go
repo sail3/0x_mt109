44c0778dd9d7f750cfcda21e42e4a9d99c39fff6
Pedro Charun <mlosail3@gmail.com>
2021-07-16 16:23:13 -0500

    dev: implements function delete pixels.

--- remove_black_pixel/main.go
+++ remove_black_pixel/main.go
@@ -5,3 +5,14 @@ import "fmt"
 func main() {
 	fmt.Println("init msg")
 }
+
+func RemoveBlackPixels(matrix [][]int) [][]int {
+	for i := 1; i < len(matrix)-1; i++ {
+		for j := 1; j < len(matrix[i])-1; j++ {
+			if matrix[i-1][j] != 1 && matrix[i+1][j] != 1 && matrix[i][j-1] != 1 && matrix[i][j+1] != 1 {
+				matrix[i][j] = 0
+			}
+		}
+	}
+	return matrix
+}
