18437b1af28d10980cca2f3de908e3bcdb0d487a
Pedro Charun <mlosail3@gmail.com>
2021-07-17 14:49:59 -0500

    dev: refactorizacion de algoritmo RemoveBlackPixels

--- remove_black_pixel/main.go
+++ remove_black_pixel/main.go
@@ -1,18 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	fmt.Println("init msg")
 }
 
 func RemoveBlackPixels(matrix [][]int) [][]int {
-	for i := 1; i < len(matrix)-1; i++ {
-		for j := 1; j < len(matrix[i])-1; j++ {
-			if matrix[i-1][j] != 1 && matrix[i+1][j] != 1 && matrix[i][j-1] != 1 && matrix[i][j+1] != 1 {
-				matrix[i][j] = 0
-			}
-		}
+	var result = make([][]int, len(matrix))
+	for i := 0; i < len(matrix); i++ {
+		result[i] = make([]int, len(matrix))
+	}
+	for i := 0; i < len(matrix); i++ {
+		result = drawPixel(result, buscarPixel(i, 0, matrix, make(map[string][]int)))
+		result = drawPixel(result, buscarPixel(0, i, matrix, make(map[string][]int)))
+		result = drawPixel(result, buscarPixel(i, len(matrix)-1, matrix, make(map[string][]int)))
+		result = drawPixel(result, buscarPixel(len(matrix)-1, i, matrix, make(map[string][]int)))
+	}
+	return result
+}
+
+func drawPixel(matrix [][]int, pixels map[string][]int) [][]int {
+	for _, v := range pixels {
+		matrix[v[1]][v[0]] = 1
 	}
 	return matrix
+}
+
+func buscarPixel(posX int, posY int, matrix [][]int, pixels map[string][]int) map[string][]int {
+	if posX < len(matrix[0]) && posY < len(matrix) && posY > -1 && posX > -1 && matrix[posY][posX] == 1 && pixels[fmt.Sprintf("%d%d", posX, posY)] == nil {
+		for _, v := range [][]int{{0, 1}, {1, 0}, {0, -1}, {-1, 0}} {
+			pixels[fmt.Sprintf("%d%d", posX, posY)] = []int{posX, posY}
+			buscarPixel(posX+v[0], posY+v[1], matrix, pixels)
+		}
+	}
+	return pixels
 }
