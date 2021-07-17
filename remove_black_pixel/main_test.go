12398df6d21bbee982daf90ca27aadeab793087f
Pedro Charun <mlosail3@gmail.com>
2021-07-17 14:43:09 -0500

    dev: agregado test adicionales

--- remove_black_pixel/main_test.go
+++ remove_black_pixel/main_test.go
@@ -46,7 +46,79 @@ func TestRemoveBlackPixels2(t *testing.T) {
 
 	result := RemoveBlackPixels(input)
 	if !reflect.DeepEqual(output, result) {
-		t.Error("remove black pixels dont passed")
+		t.Error("remove black pixels2 dont passed")
+		t.Fail()
+	}
+}
+func TestRemoveBlackPixels3(t *testing.T) {
+	input := [][]int{
+		{1, 0, 0, 0, 0, 0},
+		{0, 1, 0, 1, 1, 1},
+		{0, 0, 1, 0, 1, 0},
+		{1, 1, 0, 0, 1, 0},
+		{1, 0, 1, 1, 0, 0},
+		{1, 0, 0, 0, 0, 1},
+	}
+
+	output := [][]int{
+		{1, 0, 0, 0, 0, 0},
+		{0, 0, 0, 1, 1, 1},
+		{0, 0, 0, 0, 1, 0},
+		{1, 1, 0, 0, 1, 0},
+		{1, 0, 0, 0, 0, 0},
+		{1, 0, 0, 0, 0, 1},
+	}
+
+	result := RemoveBlackPixels(input)
+	if !reflect.DeepEqual(output, result) {
+		t.Error("remove black pixels3 dont passed")
+		t.Fail()
+	}
+}
+func TestRemoveBlackPixels4(t *testing.T) {
+	input := [][]int{
+		{1, 0, 0, 0, 0, 0},
+		{0, 1, 0, 1, 1, 1},
+		{0, 1, 0, 1, 1, 0},
+		{0, 1, 1, 0, 1, 0},
+		{0, 1, 0, 1, 0, 0},
+		{0, 0, 0, 0, 0, 1},
+	}
+
+	output := [][]int{
+		{1, 0, 0, 0, 0, 0},
+		{0, 0, 0, 1, 1, 1},
+		{0, 0, 0, 1, 1, 0},
+		{0, 0, 0, 0, 1, 0},
+		{0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 1},
+	}
+	result := RemoveBlackPixels(input)
+	if !reflect.DeepEqual(output, result) {
+		t.Error("remove black pixels4 dont passed")
+		t.Fail()
+	}
+}
+func TestRemoveBlackPixels5(t *testing.T) {
+	input := [][]int{
+		{1, 0, 0, 0, 0, 0},
+		{0, 1, 0, 1, 1, 1},
+		{0, 1, 0, 1, 1, 0},
+		{0, 1, 1, 0, 1, 0},
+		{0, 1, 0, 1, 0, 0},
+		{0, 0, 0, 0, 0, 1},
+	}
+	output := [][]int{
+		{1, 0, 0, 0, 0, 0},
+		{0, 0, 0, 1, 1, 1},
+		{0, 0, 0, 1, 1, 0},
+		{0, 0, 0, 0, 1, 0},
+		{0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 1},
+	}
+	result := RemoveBlackPixels(input)
+	if !reflect.DeepEqual(output, result) {
+		t.Error("remove black pixels4 dont passed")
 		t.Fail()
 	}
 }
